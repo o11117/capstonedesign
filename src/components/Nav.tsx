9d3f7ad84c42533f728ba3e0f598efc4cd4905e7
o11117 <gunmagician@naver.com>
2025-03-25 10:59:40 +0000

    Deploy: ad81085885739a2e3779264898da6a0a00ca30a4

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,39 +1,52 @@
 // src/components/Nav.tsx
-import { Link } from 'react-router-dom';
-import styles from '../assets/Nav.module.css';
-import logo from '/favicon.jpeg';
+import { Link } from 'react-router-dom'
+import styles from '../assets/Nav.module.css'
+import logo from '/favicon.jpeg'
 
 const Nav = () => {
   return (
     <nav className={styles.navcontainer}>
-      <div className='logocontainer'>
+      <div className="logocontainer">
         <Link to="/" className={styles.logoLink}>
-       <img src={logo} alt="메인페이지로 이동" className={styles.logo}/>
-       <div className={styles.navTitle}>CAPSTONE DESIGN</div>
-       </Link>
+          <img src={logo} alt="메인페이지로 이동" className={styles.logo} />
+          <div className={styles.navTitle}>CAPSTONE DESIGN</div>
+        </Link>
       </div>
 
-      <ul className={styles.navList}>
-        <li className={styles.navItem}>
-          <Link to="/about" className={styles.navLink}>About</Link>
-        </li>
-        <li className={styles.navItem}>
-          <Link to="/services" className={styles.navLink}>Services</Link>
-        </li>
-        <li className={styles.navItem}>
-          <Link to="/contact" className={styles.navLink}>Contact</Link>
-        </li>
-      </ul>
       <ul className={styles.navRight}>
         <li className={styles.navItem}>
-          <Link to="/login" className={styles.navLink}>Login</Link>
+          <Link to="/travelcourse" className={styles.navLink}>
+            지역별 여행 코스
+          </Link>
         </li>
         <li className={styles.navItem}>
-          <Link to="/signup" className={styles.navLink}>Sign Up</Link>
+          <Link to="/aisearch" className={styles.navLink}>
+            AI 검색
+          </Link>
         </li>
-        </ul>
+        <li className={styles.navItem}>
+          <Link to="/mytravel" className={styles.navLink}>
+            나의 여행
+          </Link>
+        </li>
+        <li className={styles.navItem}>
+          <Link to="/mypage" className={styles.navLink}>
+            마이페이지
+          </Link>
+        </li>
+        <li className={styles.navItem}>
+          <Link to="/login" className={styles.navLinkLogin}>
+            로그인
+          </Link>
+        </li>
+        <li className={styles.navItem}>
+          <Link to="/signup" className={styles.navLinkLogin}>
+            회원가입
+          </Link>
+        </li>
+      </ul>
     </nav>
-  );
-};
+  )
+}
 
-export default Nav;
+export default Nav
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,8 +1,9 @@
 import styles from '../assets/MainPage.module.css'
 import axios from 'axios'
-import {useEffect, useState} from "react"
+import { useEffect, useState } from 'react'
+import mainpic from '../../public/mainpic.jpeg'
 
-interface Data{
+interface Data {
   name: string
 }
 
@@ -11,28 +12,38 @@ const MainPage = () => {
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState(null)
 
-  useEffect(()=> {
-    axios.get('http://localhost:5001/test-query')  // 백엔드 서버 URL로 변경
-      .then(response => {
-        setData(response.data);  // 응답 데이터를 상태에 저장
-        setLoading(false);  // 로딩 상태를 종료
+  useEffect(() => {
+    axios
+      .get('http://localhost:5001/test-query') // 백엔드 서버 URL로 변경
+      .then((response) => {
+        setData(response.data) // 응답 데이터를 상태에 저장
+        setLoading(false) // 로딩 상태를 종료
       })
-      .catch(err => {
-        setError(err.message);  // 오류 발생 시 오류 메시지 저장
-        setLoading(false);
-      });
-  }, []);
+      .catch((err) => {
+        setError(err.message) // 오류 발생 시 오류 메시지 저장
+        setLoading(false)
+      })
+  }, [])
 
   if (loading) return <div>Loading...</div>
   if (error) return <div>Error: {error}</div>
   return (
-    <div className={styles.mainPage}>
-      <h1 className={styles.heading}>메인메인메인메인메인메인메인메인</h1>
-      <p className={styles.description}>내용내용내용내용내용내용내용내용내용내용내용내용내용</p>
-      <br/>
-      <p>{data[0].name}</p>
+    <div>
+      <div
+        className={styles.mainPage}
+        style={{
+          backgroundImage: `url(${mainpic})`, // import한 mainpic 사용
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+        }}>
+        <h1 className={styles.heading}>메인메인메인메인메인메인메인메인</h1>
+        <p className={styles.description}>내용내용내용내용내용내용내용내용내용내용내용내용내용</p>
+        <br />
+        <p className={styles.data}>{data[0].name}</p>
+      </div>
+      <div className={styles.HotCourseContainer}></div>
     </div>
-  );
-};
+  )
+}
 
-export default MainPage;
+export default MainPage
