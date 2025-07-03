a556f5865d12c29adfcae15708bd8a5daf9b6dc1
o11117 <gunmagician@naver.com>
2025-07-03 12:10:56 +0000

    Deploy: e0818299ad5b2bd09cf0be6ad1022ca0d40d1859

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
     strictPort: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:5001',
+        target: 'https://port-0-planit-be-mcmt59q6ef387a77.sel5.cloudtype.app',
         changeOrigin: true,
         secure: false,
       },
