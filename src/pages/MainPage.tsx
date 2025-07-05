f38376f4f78fae8b1dd866d74e592b0a5b1ab69a
o11117 <gunmagician@naver.com>
2025-07-05 10:24:58 +0000

    Deploy: 5029257d0547383c0839dc7abd9d5e36514514d5

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -46,8 +46,6 @@ const MainPage = () => {
       <div className={styles.mainPage}>
         <Hero mainpic={mainpic} />
         <HotCourses />
-        <h1 className={styles.heading}>메인메인메인메인메인메인메인메인</h1>
-        <p className={styles.description}>내용내용내용내용내용내용내용내용내용내용내용내용내용</p>
         <br />
       </div>
       <div className={styles.HotCourseContainer}></div>
