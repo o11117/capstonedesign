15dbc319e0c45f30ac35d2a2b5c94a5a9036fcc8
o11117 <gunmagician@naver.com>
2025-07-17 09:47:42 +0000

    Deploy: e0f394c10cd7d2e76b37883228c42fd2d55bcb97

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import styles from '../assets/Footer.module.css';  // CSS 모듈 import
 const Footer = () => {
   return (
     <footer className={styles.footerContainer}>
-      <p className={styles.footerText}>© 2025 KNU CAPSTONE DESIGN. All Rights Reserved.</p>
+      <p className={styles.footerText}>© 2025 PLANIT. All Rights Reserved.</p>
     </footer>
   );
 };
